0955ef10652d40bcd8eaee0fd493301f908e5299
pcbproj <pcbproj@yandex.ru>
2025-09-26 10:46:56 +0300

    output directory deleted

--- Output/Debug/Obj/mcu_1wire_lock_STM32L475/SEGGER_THUMB_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/mcu_1wire_lock_STM32L475/SEGGER_THUMB_Startup.o: \
- D:\Work\PCBteach\02_MY_projects\RADIO\mcu_1wire_lock_STM32L475\SEGGER_THUMB_Startup.s
--- Output/Debug/Obj/mcu_1wire_lock_STM32L475/stm32l475xx_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/mcu_1wire_lock_STM32L475/stm32l475xx_Vectors.o: \
- D:\Work\PCBteach\02_MY_projects\RADIO\mcu_1wire_lock_STM32L475\STM32L4xx\Source\stm32l475xx_Vectors.s
